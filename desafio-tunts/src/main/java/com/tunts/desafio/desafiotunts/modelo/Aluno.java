1242408e5bf4fe40300de61f777d38bae303716e
mirandabrandon <79466753+mirandabrandon@users.noreply.github.com>
2021-05-05 18:18:28 -0300

    Add files via upload

---
+++ desafio-tunts/src/main/java/com/tunts/desafio/desafiotunts/modelo/Aluno.java
@@ -0,0 +1,66 @@
+package com.tunts.desafio.desafiotunts.modelo;
+
+public class Aluno {
+	
+	private String nome;
+    private String matricula;
+    private double nota1;
+    private double nota2;
+    private double media;
+    private boolean aprovado;
+
+    public Aluno() { }
+
+    public Aluno(String nome, String matricula, double nota1, double nota2,
+                 double media, boolean aprovado) {
+          super();
+          this.nome = nome;
+          this.matricula = matricula;
+          this.nota1 = nota1;
+          this.nota2 = nota2;
+          this.media = media;
+          this.aprovado = aprovado;
+    }
+
+
+
+    public String getNome() {
+          return nome;
+    }
+    public void setNome(String nome) {
+          this.nome = nome;
+    }
+    public String getMatricula() {
+          return matricula;
+    }
+    public void setMatricula(String matricula) {
+          this.matricula = matricula;
+    }
+    public double getNota1() {
+          return nota1;
+    }
+    public void setNota1(double nota1) {
+          this.nota1 = nota1;
+    }
+    public double getNota2() {
+          return nota2;
+    }
+    public void setNota2(double nota2) {
+          this.nota2 = nota2;
+    }
+    public double getMedia() {
+          return media;
+    }
+    public void setMedia(double media) {
+          this.media = media;
+    }
+
+    public boolean isAprovado() {
+          return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+          this.aprovado = aprovado;
+    }
+
+}
